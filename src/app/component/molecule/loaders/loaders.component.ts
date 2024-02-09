e3a5c27d8f2d6c1d2b61690d443d61944574e7c7
naufal.adib.190403@gmail.com <naufal.adib.190403@gmail.com>
2024-02-10 01:15:51 +0700

    feat: add loaders component

---
+++ src/app/component/molecule/loaders/loaders.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loaders',
+  standalone: true,
+  imports: [],
+  templateUrl: './loaders.component.html',
+  styleUrl: './loaders.component.css'
+})
+export class LoadersComponent {
+
+}
